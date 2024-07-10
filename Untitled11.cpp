7534174769d10e60548558e83f9e92f2ff2b37f1
kartik <152079975+kartik-chauhan1@users.noreply.github.com>
2024-07-10 23:30:30 +0530

    Add files via upload

---
+++ Untitled11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the value A and B";
+    cin>>a >>b;
+    a=a*b;
+    b=a/b;
+    a=a/b;
+    cout<<"Swaping "<<a<<" "<<b;
+    
+}
